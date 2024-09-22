992c986a6eede4ac4a19b631db11a4ead3499ab0
Giang Lâm Quỳnh <gianglam.31231023875@gmail.com>
2024-09-22 20:47:56 +0700

    newfix

---
+++ GiangLam_Exercise_1/Baitapvenhatuan6.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiangLam_Exercise_1
+{
+    internal class Baitapvenhatuan6
+    {
+        public static void Main(string[] args)
+        {
+            //first exercise
+            int[] arr = { 1, 2, 4, 4, 5 };
+            double average = CalculateTheAverageValue(arr);
+            Console.WriteLine($"The average value of the array is: {average} ");
+            Console.WriteLine();
+            //second exercise
+            int value = int.Parse(Console.ReadLine());
+            bool specific = TestIfContainSpecificValue(arr, value);
+            Console.WriteLine(specific ? $"The array contain {value}" : $"The array doesn't contain {value}");
+            Console.WriteLine();
+            //third exercise
+            int element = int.Parse(Console.ReadLine());
+            int index = FindTheIndexOfElement(arr, element);
+            if (index != -1)
+            {
+                Console.WriteLine($"The index of the value {element} is {index}");
+            }
+            else
+            {
+                Console.WriteLine($"Value {element} is not found in the array");
+            }
+            Console.WriteLine();
+            //forth exercise
+            int removeElement = int.Parse(Console.ReadLine());
+            bool respeel = RemoveSpecificElementFromAnArray(arr, removeElement);
+            Array.Resize(ref arr, arr.Length - 1);
+            PrintRemoveArray(arr);
+            Console.WriteLine();
+            //fifth exercise
+            PrintMaxMinnumber(arr);
+            Console.WriteLine();
+            //sixth exercise
+            ReverseArray(arr);
+            PrintReverseArray(arr);
+            Console.WriteLine();
+            //seventh exercise 
+            FindDuplicateValue(arr);
+            //eight exercise
+            //nt b = arr[i];
+            //RemoveDuplicateElement(arr, b);
+            //ninth exercise
+            BubbleSort(arr);
+            //tenth exercise
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter sentence:");
+                string sentence = Console.ReadLine();
+                if (sentence == null)
+                {
+                    Console.WriteLine("Please enter in the correct form of a sentence");
+                    Console.WriteLine("Enter word");
+                    string word = Console.ReadLine();
+                    bool result = CheckWordExistInSentence(sentence, word);
+                    Console.WriteLine(result ? "Word appears" : "Word doesn't appear");
+                }
+            }
+
+
+        }
+        //first exercise
+        static double CalculateTheAverageValue(int[] arr)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //arr[i] = rnd.Next(1,50);
+                //Console.WriteLine($"Computer number is: {arr[i]} ");
+                sum += arr[i];
+
+            }
+            double average = sum / arr.Length;
+            return average;
+        }
+        //second exercise
+        static bool TestIfContainSpecificValue(int[] arr, int value)
+        {
+            foreach (int i in arr)
+            {
+
+                //Console.WriteLine($"Computer number is: {i}");
+                if (i == value)
+                    return true;
+            }
+            return false;
+        }
+        //third exercise
+        static int FindTheIndexOfElement(int[] arr, int element)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //Console.WriteLine($"Computer number is: {arr[i]}");
+                if (arr[i] == element)
+                    return i;
+            }
+            return -1;
+        }
+        //forth exercise
+        static bool RemoveSpecificElementFromAnArray(int[] arr, int speElement)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == speElement)
+                {
+                    for (int j = i; j < arr.Length - 1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                    return true;
+
+                }
+            }
+            return false;
+        }
+        static void PrintRemoveArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        //fifth exercise
+        static void PrintMaxMinnumber(int[] arr)
+        {
+            int max = arr[0], min = arr[0];
+            foreach (int val in arr)
+            {
+                if (val > max) max = val;
+                if (val < min) min = val;
+
+            }
+            Console.WriteLine($"Min of the array is: {min}; max of the array is: {max} ");
+        }
+        //sixth exercise
+        static void ReverseArray(int[] arr)
+        {
+            int start = 0;
+            int end = arr.Length - 1;
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        static void PrintReverseArray(int[] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        //seventh exercise
+        static void FindDuplicateValue(int[] arr)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        Console.WriteLine($"The duplicate value is: {arr[i]}");
+                        count++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no duplicate value in the array");
+                        break;
+                    }
+
+                }
+
+            }
+            return;
+        }
+        //eighth exercise
+
+        static void RemoveDuplicateElement(int[] arr, int b)
+        {
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        Console.WriteLine($"The duplicate value is: {arr[i]}");
+                        count++;
+                        b = arr[i];
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no duplicate value in the array");
+                    }
+                    break;
+                }
+            }
+
+            RemoveSpecificElementFromAnArray(arr, b);
+            PrintRemoveArray(arr);
+            Console.WriteLine();
+        }
+        //ninth exercise
+        static void BubbleSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        //tenth exercise
+        static bool CheckWordExistInSentence(string sentence, string word)
+        {
+            char[] s = sentence.ToCharArray();
+            char[] w = word.ToCharArray();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < w.Length; j++)
+                {
+                    if (s[i] == w[j])
+                    {
+                        return true;
+                    }
+
+                }
+            }
+            return false;
+        }
+    }
+}
--- GiangLam_Exercise_1/baitapvenhatuan4_11bai.cs
+++ GiangLam_Exercise_1/baitapvenhatuan4_11bai.cs
@@ -13,7 +13,12 @@ namespace GiangLam_Exercise_1
             baitap01();
             baitap02();
             baitap03();
+            baitap04();
             baitap05();
+            baitap06();
+            baitap08();
+            baitap09();
+            baitap10();
         }
         static void baitap01()
         {
@@ -43,7 +48,23 @@ namespace GiangLam_Exercise_1
             double x = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter y coordinate: ");
             double y = double.Parse(Console.ReadLine());
-            Console.WriteLine($"The coordinate point {x},{y} lies in {(x > 0 ? (y > 0 ? "First" : "Fourth") : (y > 0 ? "Second" : (x < 0 && y < 0 ? "Third" : "on an axis")))} Quadrant");
+            Console.WriteLine($"The coordinate point {x},{y} lies in {(x > 0 ? (y > 0 ? "First" : "Fourth") :
+                (y > 0 ? "Second" : (x < 0 && y < 0 ? "Third" : "Origin")))} Quadrant");
+        }
+        static void baitap04()
+        {
+            Console.WriteLine("Enter number for three sides of the triangle: ");
+            double a = int.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+            double c = int.Parse(Console.ReadLine());
+            if (a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0)
+            {
+                Console.WriteLine($"The triangle is: {((a == b && b == c) ? "Equilateral" : (a == b || b == c || a == c) ? "Isosceles" : "Scalene")} ");
+            }
+            else
+            {
+                Console.WriteLine("This is not a triangle");
+            }
         }
         static void baitap05()
         {
@@ -63,6 +84,121 @@ namespace GiangLam_Exercise_1
             }
             Console.WriteLine("Average of 10 numbers is: " + average + ". Sum of 10 number is: " + answer);
         }
-    }
+        static void baitap06()
+        {
+            Console.WriteLine("Enter a number: ");
+            int m = int.Parse(Console.ReadLine());
+            int answer = 0;
+            Console.WriteLine("The multiplication table of a given integer is: ");
+            for (int i = 1; i <= 10; i++)
+            {
+                answer = m * i;
+                Console.WriteLine($"{m} x {i} = {answer}");
+            }
+        }
+        static void baitap07()
+        {
+            Console.WriteLine("Enter number to display a pattern triangles");
+            int number = 4;
+            int r_1 = 1;
+            int r_2 = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(r_1 + " ");
+                    r_1++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = i; j <= number + 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int g = 1; g <= i; g++)
+                {
+                    Console.Write(r_2 + " ");
+                    r_2++;
+                }
+                Console.WriteLine();
+            }
+        }
+        static void baitap08()
+        {
+            Console.WriteLine("Enter n terms of harmonic series: ");
+            double har_num = double.Parse(Console.ReadLine());
+            double sum_har_num = 1;
+            Console.Write("1");
+            for (int i = 2; i <= har_num; i++)
+            {
+                Console.Write(" + ");
+                Console.Write($" 1/{i} ");
+                sum_har_num += 1.0 / i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum of n terms of harmonic series: {sum_har_num} ");
+        }
+        static void baitap09()
+        {
+            Console.WriteLine("Enter a given number: ");
+            int g = int.Parse(Console.ReadLine());
+            int answer = 0;
+            for (int i = 1; i < g; i++)
+            {
+                if (g % i == 0)
+                {
+                    answer += i;
+                }
+            }
+            if (answer == g)
+            {
+                Console.WriteLine("The given number is perfect.");
+            }
+            else
+            {
+                Console.WriteLine("The given number is not perfect.");
+            }
+        }
+        static void baitap10()
+        {
+            Console.WriteLine("Enter a number to determine whether it is prime or not: ");
+            int p = int.Parse(Console.ReadLine());
+            int count = 0;
+            if (p < 2)
+            {
+                Console.WriteLine("The number is not prime.");
+            }
+            for (int i = 2; i < p; i++)
+            {
+                if (p % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("The number is prime.");
 
+            }
+            else
+            {
+                Console.WriteLine("The number is not prime.");
+            }
+            ///
+
+        }
+
+    }
 }
